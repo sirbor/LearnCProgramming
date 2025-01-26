b38b9fb29d6e5ba0209b5b664ad1192b38c63403
Dominic Bor <dominicbor44@gmail.com>
2025-01-26 17:04:11 +0300

    Continous learning - basics, control statements

--- factorial.c
+++ factorial.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 int factorial(int num) {
-  if(num < 0) {
-    printf("Factorial of a negative integer is not precedented\n");
+  if (num < 0) {
+    printf("Factorial of 0 is 1\n");
     return -1;
-  } if(num == 0 || num == 1) {
+  } if(num == 0 || num ==1) {
     return 1;
-  }
+  } 
   int result = 1;
-  for(int i = 2; i <= num; i++){
+  for(int i = 2; i <= num; i++) {
     result *= i;
   }
   return result;
 }
 
-int main() {
+int main(void) {
   int n;
-  printf("PLease enter a non - negative integer\n");
+  printf("Enter a number: ");
   scanf("%d", &n);
 
-  int fact = factorial(n);
+  int fact= factorial(n);
   if(fact != -1) {
-    printf("Factorial of %d is %d\n", n , fact);
+    printf("The factorial of %d is %d", n, fact);
   }
   return 0;
 }
